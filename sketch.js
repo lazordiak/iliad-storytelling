fa9d659562465cc9f8491fa528a6e2bec3696af5
Schuyler deVos <schuylerdevos@gmail.com>
2018-06-07 19:14:38 -0400

    experimenting with NYT API
    circles are okay, API data -- not so much

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
+var urldata;
+
+function preload() {
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=30c136eed7bb433ea12dbdbf188246c5&q=dead%20war&begin_date=20180606&fl=headline";
+  urldata = loadJSON(url);
+}
+
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.style('display', 'block');
   angleMode(DEGREES);
-  background(0);
-
-
+  noLoop();
 }
 
 function windowResized() {
@@ -12,7 +17,6 @@ function windowResized() {
 }
 
 /*
-
 CHARACTERS:
 Argives:
 achilles - impetuous, vicious, sees far - blood red
@@ -27,14 +31,9 @@ paris - extravagant, cowardly, charming - orange
 aeneas - nobleman, valiant, stony - brown
 polydamas - cool and cautious lieutenant - gray
 helen - wistful, distant - white
-
 */
 
-// so lets say a grid of vertices represents each character
-//then we can crawl data and apply it to each of the... vertices according
-// to conditions? this will probably turn out to be the single shittiest way
-// one could get this done
-
+/*
 var achilles = [];
 var agamemnon = [];
 var patroclus = [];
@@ -50,21 +49,53 @@ var helen = [];
 var aegeans = [achilles,agamemnon,patroclus,odysseus,menelaus];
 
 var trojans = [hector,paris,aeneas,polydamus,helen];
+*/
 
 function draw() {
-  background(0);
 
-  for (i = 0; i < aegeans.length; i++) {
+  background(200);
+
+  var headlines = urldata.response.docs;
+
+  var firstHeadline = headlines[0].main;
+
+  createP(firstHeadline);
+
+  console.log(firstHeadline);
+
+  textAlign(CENTER);
+  text(firstHeadline, 0, height - 30, width, 30);
+
+/*
+DEPRECATED BUT HERE SO I DONT FORGET IT
+for (i = 0; i < 10; i++) {
 
     beginShape();
 
     for (let a = 0; a < 360; a += 10) {
-      let x = 50 * sin(a) + (width*.5);
-      let y = 50 * cos(a) + (height*.5);
+      let x = 60 * sin(a) + (width*.5);
+      let y = 60 * cos(a) + (height*.5);
       curveVertex(x, y);
     }
 
     endShape(CLOSE);
 
   }
+  */
+
+//drawing the polygon
+//polygon(width/2,height/2, 50, 15);
+
+}
+
+//polygon class
+function polygon(x, y, radius, npoints) {
+  var angle = 360 / npoints;
+  beginShape();
+  for (let a = 0; a < 360; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
