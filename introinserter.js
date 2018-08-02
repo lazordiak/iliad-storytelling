c2d990ef2e29ad9563be748680551bcf8fd0ec44
Schuyler deVos <schuylerdevos@gmail.com>
2018-08-02 08:53:00 +0800

    building probability distribution

--- introinserter.js
+++ introinserter.js
@@ -61,63 +61,59 @@ var helen = {
 };
 
 //list of all the character-objects to iterate through
-characterList = [achilles,agamemnon,nestor,odysseus,diomedes,
-    hector,paris,polydamas,priam,helen]
+characterList = [hector,priam,helen,paris,polydamas,odysseus,achilles,agamemnon,
+  diomedes,nestor];
+
+//the total score of all characters, initialized to zero
+//since we select different books each go through this will be
+//different every time
+totalScore = 0;
+
+//variable to store the probability distributions
+weights = []
 
 //***************************STORY POSSIBILITIES********************************
 //THESE ARE THE POSSIBLE FILL-INS TO ALL THE BLANKS IN THE STORY
 //pos[x] refers to the empty spaces in the html template
 
-var pos0 = [{
-  'Trojans': {
+var pos0 = {
     'Hector': "Sing to me, as you will",
     'Priam': "Recall with sadness",
     'Helen': "Let us ponder on",
     'Paris': "We'll speak, reluctantly",
-    'Polydamas': "We will reflect on"
-  },
-  'Greeks': {
+    'Polydamas': "We will reflect on",
     'Odysseus': "Explain to us",
     'Achilles': "Let us remember the glory of",
     'Agamemnon': "Recall the damned thing",
     'Diomedes': "Sing to me",
     'Nestor': "Let us recall, at length"
-  }
-}];
+};
 
-var pos1 = [{
-  'Trojans': {
+var pos1 = {
     'Hector': "barbaric",
     'Priam': "merciless",
     'Helen': "impressive",
     'Paris': "frightening",
-    'Polydamas': "dreadful"
-  },
-  'Greeks': {
+    'Polydamas': "dreadful",
     'Odysseus': "illogical",
     'Achilles': "glorious",
     'Agamemnon': "vicious",
     'Diomedes': "violent",
     'Nestor': "tempestuous -- like a sea-storm, maybe, or else a typhoon making landfall --"
-  }
-}];
+};
 
-var pos2 = [{
-  'Trojans': {
+var pos2 = {
     'Hector': "Sing to me, as you will",
     'Priam': "Recall with sadness",
     'Helen': "Let us ponder on",
     'Paris': "We'll speak, reluctantly",
-    'Polydamas': "We will reflect on"
-  },
-  'Greeks': {
+    'Polydamas': "We will reflect on",
     'Odysseus': "the boisterous",
     'Achilles': "the hero",
     'Agamemnon': "Recall the damned thing",
     'Diomedes': "Sing to me",
     'Nestor': "Let us recall, at length"
-  }
-}]
+};
 
 //************************INITIALIZING W/PROCESSING****************************
 // Variable to store json data
@@ -145,9 +141,9 @@ function setup() {
 
   //check to see if our html injector is still functioning, this is the code
   //we use to throw our text into the html template
-  allpos = pos0.concat(pos1,pos2);
+  allpos = [pos0,pos1,pos2];
   for (var i = 0; i < allpos.length; i++) {
-    character = allpos[i]["Greeks"]["Odysseus"]
+    character = allpos[i]["Odysseus"]
     document.getElementById("pos"+i.toString()).textContent="HAHA THE ILIAD";
   }
 
@@ -169,7 +165,6 @@ function setup() {
     //arrays are easier to work with so this puts all the character keywords in
     //array form (greed/pride/etc)
     bookWordsInArray = Object.keys(randomBookWords);
-    console.log(bookWordsInArray);
     //now here's the kicker -- for every word in our random book dataset
     for (var j=0; j<bookWordsInArray.length; j++) {
       //if the current character's word array property inside the character
@@ -177,37 +172,46 @@ function setup() {
       if (characterList[i]['words'].includes(bookWordsInArray[j])) {
         //then we want to add the score associated with that key to the
         //character's power score
-        console.log(theData[randomBook][bookWordsInArray[j]]);
+        characterList[i]['score'] = characterList[i]['score'] + theData[randomBook][bookWordsInArray[j]];
+        //console.log(characterList[i]['score']);
+        totalScore = totalScore + theData[randomBook][bookWordsInArray[j]];
       }
 
     }
 
   }
 
+//*******gooder shit -- selecting appropriate verses through power scores*******
+
+/*boom, ive got it...
+if this will work.
+what i want to do is turn the power scores into a probability distribution
+kind of like in those nlp neural networks
+and then we choose a character based on that probability distribution.
+... how do i make a probability distribution
+
+ok
+*/
+//turning raw score into percentage of total score and pushing it to
+//weights i.e. creating the probability distribution
+for (var i=0; i<characterList.length; i++) {
+  characterList[i]['score'] = characterList[i]['score']/totalScore
+  weights.push(characterList[i]['score']);
+  console.log(weights);
+};
+
+//for each position uses the probability to select a character
+for (var i=0; i < allpos.length; i++) {
+  continue
+}
+
 }
 
 /*
 to do:
 
-inject from the word things, like the json, instead of by hand
-
-we have json of the pure word data
-need to associate it with the characters
-so we could define each character as an array and then check
-to see if each word is in the array of each character
-if so, add that word count score to the character's score
-
-ok so:
-
-each character has to be associated with both a book and a list of words
-so key, and the thing is an array of words and a book
-for each character
-select a random book
-assign that book to them
-then for each word in the book list
-if its in the character's array
-then we add that number to the CHARACTER POWER SCORE
-then we can compare CHARACTER POWER SCOREs
-
-book title[word]
+implement method for selecting character
+finish all possible character implementations for all positions
+make it pretty
+make sure all these files work together
 */
